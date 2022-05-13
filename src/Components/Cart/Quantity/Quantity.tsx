e76bf97e81bcfc0c99d87b9f10461a894617a16d
girts521 <girts521@gmail.com>
2022-05-13 17:54:04 +0200

    finished product layout

---
+++ src/Components/Cart/Quantity/Quantity.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import {Container} from "./styles";
+
+const Quantity: React.FC = () => {
+
+        //state for product quantity
+        const [quantity, setQuantity] = useState(1);
+
+        //function to add product quantity
+        const addQuantity = () => {
+            //if quantity is less than 10, add 1 to quantity
+            if (quantity < 10) {
+            setQuantity(quantity + 1);
+            }
+            else{
+                return
+            }
+        };
+    
+        //function to subtract product quantity
+        const subtractQuantity = () => {
+            //if quantity is 1, do not subtract
+            if (quantity === 1) {
+                return;
+            }
+            //else subtract
+            else{
+                setQuantity(quantity - 1);
+            }
+        }
+    
+
+    return (
+        <Container>
+        <button onClick={subtractQuantity}>-</button>
+        <input type="number" min={1} value={quantity} />
+        <button onClick={addQuantity}>+</button>
+        </ Container>
+    )
+
+}
+
+export default Quantity;
--- src/Pages/ProductStyles.tsx
+++ src/Pages/ProductStyles.tsx
@@ -1,12 +1,111 @@
 import styled from "styled-components";
+import Carousel from "../Components/Carousel/Carousel";
+import {Container as CarouselContainer}  from "../Components/Carousel/styles";
 
 export const Container = styled.main`
     background: linear-gradient(318deg, #f8f9fa, #e9ecef);
     background-size: 200% 200%;
-    padding-top:100px;
-    height:100vh;
+    min-height:95vh;
 
     h1{
         margin-top: 50px;
+        padding-top:20px;
+        text-align: center;
+        font-size: 2rem;
+        font-weight: bold;
+        color: #212529;
+
+    }
+`;
+
+export const ProductInfo = styled.div`
+    display:flex;
+    flex-direction:column;  
+    padding-bottom: 100px;
+`;
+
+export const AddToCart = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    margin-top:20px;
+    position: fixed;
+    bottom: 0px;
+    width: 100%; 
+    background-color: #e9ecef;
+    
+    button {
+    border: none;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+    width: 90%;
+    height: 50px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.8;
+        transform: scale(1.01);
+    }
+  }
+
+`;
+
+export const Price = styled.div`
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #212529;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
+`;
+
+export const SmallInfo = styled.div`
+    font-size: 1rem;
+    color: #212529;
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
+`;
+
+export const Size = styled.div`
+    font-size: 1rem;
+    color: #212529;
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
+    border: 1px solid #212529;
+    border-radius: 5px;
+    padding: 5px;
+    width: 50px;
+    text-align: center;
+
+
+
+    &:hover {
+        cursor: pointer;
+        background-color: #212529;
+        color: white;
+    }
+
+`;
+
+export const Sizes = styled.div`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 10px;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    width: 100%;
+    height: 100px;
+    margin: 0 auto;
+
+    ::-webkit-scrollbar{
+       display: none;
     }
 `;
